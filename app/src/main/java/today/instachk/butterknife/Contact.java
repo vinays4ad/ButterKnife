5b869a7ffa74cf94cdb43f79c6b2291fb1211843
vinays4ad <vinays4ad@gmail.com>
2018-10-24 14:59:44 -0500

    Sample ButterKnife Project

---
+++ app/src/main/java/today/instachk/butterknife/Contact.java
@@ -0,0 +1,31 @@
+package today.instachk.butterknife;
+
+/**
+ * Created by ravi on 05/10/17.
+ */
+
+public class Contact {
+    String name;
+    String mobile;
+
+    public Contact(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
